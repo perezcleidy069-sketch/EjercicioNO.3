bdd3dad4281f8c81b8fd83033ba5ca9def231958
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-04-27 15:04:54 -0600

    feat:

--- 03-ejercicio.js
+++ 03-ejercicio.js
@@ -2,13 +2,13 @@
 console.log("---Crear un objeto Estudiante---");
 
 const estudiante={
-    nombre:"Alejandra",
+    name:"Alejandra",
     apellido:"López",
     edad:23,
     cursos:"Mate"
 }
 console.log(estudiante);
-let { curso: materia, nombre}=estudiante;
+let {curso: materia, name}=estudiante;
 console.log(materia)
 
 //2. Lista de estudiantes
@@ -42,4 +42,42 @@ const estudiantes=[
     }
 ]
 console.log(estudiantes)
+for (let i of estudiantes){
+    console.log(i.nombre)
+}
+//Promedio de notas por estudiante
+console.log("---Promedio de notas por estudiante---")
+const students=[
+    id={
+        nam:"Esteban",
+        notas:[60, 70, 45, 23,56]
+    },
+    id={
+        nam:"Pedro",
+        notas:[60, 70, 95, 45,56]
+    },
+    id={
+        nam:"Fabiola",
+        notas:[90, 70, 95, 93,56]
+    },
+    id={
+        nam:"Estela",
+        notas:[66, 90, 45, 93,66]
+    },
+    function calcularNota(){
+        for (let e=0; e<notas; e++){
+            const total=0;
+            total=total + notas[e];
+            console.log(total);
+        
+        }
+    }
+]
 
+//4.Buscar el estudiante mayor
+console.log("---Buscar el estudiante mayour---")
+console.log(estudiantes)
+for (let d=0; d<edad.length; d++){
+    let mayor=0;
+    
+}
